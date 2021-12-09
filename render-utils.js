364068d39339d6ba649dd91562c73384fc0e971c
Ari Harlem-Caballero <ari.bash.harlem@gmail.com>
2021-12-09 11:16:45 -0800

    think i finished render-utils

--- render-utils.js
+++ render-utils.js
@@ -9,26 +9,28 @@ export function renderFriend(friend) {
     // make three elements: an outer div, a place for the name, and a place for an emoji
     const friendDiv = document.createElement('div');
     const friendName = document.createElement('p');
-    const friendLevel = document.createElement('p');
+    const friendFace = document.createElement('p');
     // add friend, name, and emoji classes to the appropriate elements
-
+    friendDiv.classList.add('friends');
+    friendName.classList.add('friend-name');
+    friendFace.classList.add('friend-face');
     // put the friend's name in the nameEl
     friendName.textContent = friend.name;
     
     // for each friend, set the emojiEl's text content to a different emoji depending on their satisfaction level
-    friendLevel.textContent = friend.level
+    friendFace.textContent = friend.satisfaction < 0 ? 0 : friend.satisfaction;
     
-    if (friend.level === 0) {
+    if (friend.satisfaction === 0) {
         '😩';
-    } if (friend.level === 1) {
+    } if (friend.satisfaction === 1) {
         '😐';
-    } if (friend.level === 2) {
+    } if (friend.satisfaction === 2) {
         '😃';
     } else {
         '😆';
     }
     // append the emojiEl and nameEl to the outer div
-    friendDiv.append(friendName, friendLevel);
+    friendDiv.append(friendName, friendFace);
     // return the outer div
     return friendDiv;
 }
